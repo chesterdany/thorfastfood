f776c843be5708293f5b2cb9c2fa63a056110d58
Brendea Daniel <chesterdany22@yahoo.com>
2022-06-04 15:40:10 +0300

    basic structure added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import Nav from "./components/Nav";
+import Body from "./components/Body";
+import Fotter from "./components/Fotter";
 
 function App() {
   return (
     <div className="App">
-      Thor
+      <Nav/>
+      <Body/>
+      <Fotter/>
     </div>
   );
 }
